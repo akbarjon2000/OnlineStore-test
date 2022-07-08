dc0f36c09e2938a9a8bbca165f027d4409c077c6
akbarjon2000 <1543431a@gmail.com>
2022-07-08 18:00:00 +0900

    creating the modal

--- assets/application.js
+++ assets/application.js
@@ -7,3 +7,44 @@ if (document.getElementById('sort_by') !== null) {
         window.location = url.href;
     })
 }
+
+const CountrySelector = document.getElementById("addressCountryNew")
+if (CountrySelector != null) {
+    CountrySelector.addEventListener('change', (e) => {
+        console.log(e)
+
+        var provinces = this.Option[e.selectedIndex].getAttribute("data-provinces");
+        var ProvinceSelector = document.getElementById("addressProvinceNew")
+        var arrayProvinces = JSON.parse(provinces)
+
+        console.log(arrayProvinces)
+    })
+}
+
+if (document.getElementById("forgetPass") != null) {
+    document.addEventListener("click", () => {
+        var forgetPassForm = document.getElementById("forgetPasswordCon")
+        if (forgetPassForm.classList.contains("d-none")) {
+            forgetPassForm.classList.remove("d-none")
+        }
+    })
+}
+
+const productInfoAnchors = document.querySelectorAll("#product-info-anchor");
+const productModal = new bootstrap.Modal(document.getElementById("product-info-modal"))
+if (productInfoAnchors.length > 0) {
+    productInfoAnchors.forEach((item) => {
+        item.addEventListener("click", (e) => {
+            e.preventDefault()
+
+            var url = '/products/' + item.getAttribute('product-handle') + '.jsS'
+
+            fetch(url)
+                .then(resp => resp.json())
+                .then(function (data) {
+                    console.log(data)
+                })
+            productModal.show()
+        })
+    })
+}
