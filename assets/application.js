35228c159d2509e3a87e0f0110d86543c4aec077
akbarjon2000 <1543431a@gmail.com>
2022-07-10 11:34:22 +0900

    moving from 1.0.0 to 2.0.0

--- assets/application.js
+++ assets/application.js
@@ -31,20 +31,104 @@ if (document.getElementById("forgetPass") != null) {
 }
 
 const productInfoAnchors = document.querySelectorAll("#product-info-anchor");
-const productModal = new bootstrap.Modal(document.getElementById("product-info-modal"))
+var productModal;
+if (document.getElementById("product-info-modal") !== null) {
+    productModal = new bootstrap.Modal(document.getElementById("product-info-modal"), {})
+
+}
 if (productInfoAnchors.length > 0) {
     productInfoAnchors.forEach((item) => {
         item.addEventListener("click", (e) => {
             e.preventDefault()
 
-            var url = '/products/' + item.getAttribute('product-handle') + '.jsS'
+            var url = '/products/' + item.getAttribute('product-handle') + '.js'
+            var price = item.getAttribute('product-price')
 
             fetch(url)
                 .then(resp => resp.json())
                 .then(function (data) {
                     console.log(data)
-                })
+                    document.getElementById("modal-image").src = data.images[0];
+                    document.getElementById("productTitle").innerHTML = data.title;
+                    document.getElementById("productPrice").innerHTML = price
+                    document.getElementById("productDescription").innerHTML = data.description;
+
+                    var variants = data.variants
+                    var select = document.getElementById("productSelect");
+
+                    variants.forEach(variant => {
+                        select.options[select.options.length] = new Option(variant.option1, variant.id)
+                    })
+                });
             productModal.show()
         })
+    })
+}
+
+var form = document.getElementById("addToCartForm")
+
+if (form !== null) {
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        var formBody = {
+            'items': [
+                {
+                    'id': document.getElementById("productSelect").value,
+                    'quantity': document.getElementById("quantity").value
+                }
+            ]
+        }
+
+        fetch("/cart/add.js", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formBody)
+        })
+            .then(resp => resp.json())
+            .then(() => updateCart())
+            .catch(err => {
+                console.error("Error: " + err)
+            })
+    })
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    updateCart()
+})
+
+const updateCart = () => {
+    fetch('/cart.js')
+        .then(resp => resp.json())
+        .then(data => {
+            document.getElementById("item-count").innerHTML = data.item_count
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
+
+var searchInput = document.getElementById("search-input");
+
+if (searchInput !== null) {
+    searchInput.addEventListener("input", () => {
+        console.log(searchInput.value)
+        fetch(`/search/suggest.json?q=${searchInput.value}&resources[type]=product`)
+            .then(resp => resp.json())
+            .then(data => {
+                const { products } = data.resources.results;
+                console.log(products)
+                var suggest_modal;
+                if (document.getElementById("offcanvasRight") !== null) {
+                    suggest_modal = new bootstrap.Offcanvas(document.getElementById("offcanvasRight"))
+                }
+                if (products.length > 0) {
+                    suggest_modal.show()
+                }
+            })
     })
 }
