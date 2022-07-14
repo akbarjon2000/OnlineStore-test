728f311d5989d2312c6c28efe19c15bff45a992e
akbarjon2000 <1543431a@gmail.com>
2022-07-14 22:56:23 +0900

    customizing custom mouse

--- assets/application.js
+++ assets/application.js
@@ -152,5 +152,129 @@ if (searchInput !== null) {
                     }, 2000)
                 }
             })
+
     })
 }
+
+var product = {
+    color: "",
+    connection: "",
+    pad: ""
+}
+
+var id = "bl"
+
+const black = document.getElementById("black")
+const white = document.getElementById("white")
+const wire = document.getElementById("wire")
+const wireless = document.getElementById("wireless")
+const linear = document.getElementById("linear")
+const square = document.getElementById("square")
+
+black.addEventListener("click", () => {
+    if (product.color === "white" || product.color === "") {
+        product.color = "black"
+    }
+    else {
+        product.color = ""
+    }
+    cases(product)
+    // console.log(product)
+})
+
+white.addEventListener("click", () => {
+    if (product.color === "black" || product.color === "") {
+        product.color = "white"
+    }
+    else {
+        product.color = ""
+    }
+    cases(product)
+    // console.log(product)
+})
+
+wire.addEventListener("click", () => {
+    if (product.connection === "wireless" || product.connection === "") {
+        product.connection = "wire"
+    }
+    else {
+        product.connection = ""
+    }
+    cases(product)
+    // console.log(product)
+})
+
+wireless.addEventListener("click", () => {
+    if (product.connection === "wire" || product.connection === "") {
+        product.connection = "wireless"
+    }
+    else {
+        product.connection = ""
+    }
+    cases(product)
+    // console.log(product)
+})
+
+linear.addEventListener("click", () => {
+    if (product.pad === "square" || product.pad === "") {
+        product.pad = "linear"
+    }
+    else {
+        product.pad = ""
+    }
+    cases(product)
+    // console.log(product)
+})
+
+square.addEventListener("click", () => {
+    if (product.pad === "linear" || product.pad === "") {
+        product.pad = "square"
+    }
+    else {
+        product.pad = ""
+    }
+    cases(product)
+    // console.log(product)
+})
+
+function cases(newProduct) {
+    // console.log(newProduct.color + "-" + newProduct.connection + "-" + newProduct.pad)
+    switch (newProduct.color + "-" + newProduct.connection + "-" + newProduct.pad) {
+        case "":
+            console.log("empty");
+            break;
+        case "black--":
+            console.log("black-image")
+            break;
+        case "white--":
+            console.log("white-image")
+            break;
+        case "black-wire-":
+            console.log("black-wire-")
+            break;
+        case "black-wireless-":
+            console.log("black-wireless-image")
+            break;
+        case "black--square":
+            console.log("black-wire-square")
+            break;
+        case "black--linear":
+            console.log("black-wire-linear")
+            break;
+        case "black-wire-linear":
+            console.log("black-wire-linear-image")
+            break;
+        case "black-wireless-linear":
+            console.log("black-wireless-linear-image")
+            break;
+        case "black-wire-square":
+            console.log("black-wire-square-image")
+            break;
+        case "black-wireless-square":
+            console.log("black-wireless-square-image")
+            break;
+        default:
+            console.log("white")
+    }
+}
+
